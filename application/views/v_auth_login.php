02374754f74a46e25117c3f8a114efcaa7ee94a5
stephanus yogi <stephanusyogi12@gmail.com>
2023-06-21 13:07:21 +0700

    progress

--- application/views/v_auth_login.php
+++ application/views/v_auth_login.php
@@ -74,7 +74,7 @@
                 <!-- BEGIN: Login Form -->
                 <div class="h-screen xl:h-auto flex py-5 xl:py-0 my-10 xl:my-0">
                     <div class="my-auto mx-auto xl:ml-20 bg-white dark:bg-darkmode-600 xl:bg-transparent px-5 sm:px-8 py-8 xl:p-0 rounded-md shadow-md xl:shadow-none w-full sm:w-3/4 lg:w-2/4 xl:w-auto">
-                        <img alt="" class="intro-x w-56 mb-2" src="<?= base_url() ?>assets/images/logoBI.png">
+                        <img alt="" class="intro-x w-1/2 mb-2" src="<?= base_url() ?>assets/images/logoBI.png">
                         <div class="intro-x mt-2 text-slate-400 xl:hidden text-center">Pension Fund Tracker</div>
                         <form action="<?= base_url() ?>login-verification" method="post">
                           <div class="intro-x mt-8">
